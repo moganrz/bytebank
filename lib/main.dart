5721d1e36087ced40415ad3a2f57bf03b71d97f1
Mogan Xumerle <moganrz@gmail.com>
2020-03-04 22:47:56 -0300

    Adicinando cards com valor e conta  de transferencia

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,55 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('Transferências'),
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Icon(Icons.add),
-    ),
-  ),
-));
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          body: ListaTransferencias(),
+          appBar: AppBar(
+            title: Text('Transferências'),
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {},
+          ),
+        ),
+      ),
+    );
+
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      ItemTransferencia(Transferencia(100, 1000)),
+      ItemTransferencia(Transferencia(200, 1000)),
+      ItemTransferencia(Transferencia(100, 1000)),
+      ItemTransferencia(Transferencia(500, 1000)),
+      ItemTransferencia(Transferencia(100, 1000)),
+      ItemTransferencia(Transferencia(300, 3000)),
+    ]);
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: ListTile(
+      leading: Icon(Icons.monetization_on),
+      title: Text(_transferencia.valor.toString()),
+      subtitle: Text(
+        _transferencia.numeroConta.toString(),
+      ),
+    ));
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
